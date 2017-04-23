8f9577ffe4f6c91b8c1265fa3516d8b7bf525bcf
Som Euakarn Liengtiraphan <eliengti@u.rochester.edu>
2017-04-23 16:38:58 -0400

    Ideal layout completes
    Note: finished jane goodall page.
    don’t forget nav bar and header and footer

---
+++ js/template.js
@@ -0,0 +1,14 @@
+// Closes the sidebar menu
+    $("#menu-close").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+    
+
+// Opens the sidebar menu
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+
+//scroll next section
