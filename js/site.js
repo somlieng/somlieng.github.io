18ebea06a1bb93ba652a58d997a03ee50d8e1753
Som Euakarn Liengtiraphan <eliengti@u.rochester.edu>
2017-05-01 18:55:07 -0400

    made template for the index/homepage
    next time: customize the page to include latest inserts for the blog

---
+++ js/site.js
@@ -0,0 +1,34 @@
+
+//from the grayscale.js file of http://bit.ly/220bv7T
+// jQuery to collapse the navbar on scroll
+function collapseNavbar() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+}
+
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
+
+//from the grayscale.js file of http://bit.ly/220bv7T
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+//from the grayscale.js file of http://bit.ly/220bv7T
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+  if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
+    $('.navbar-toggle:visible').click();
+  }
+});
+
